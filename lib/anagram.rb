b775d3c7d21df62563d469409a40e2e468a08afb
ralph1786 <ralph1786@gmail.com>
2018-12-16 10:58:27 -0500

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array_of_word)
+    #Use select to return any possible matches in the array, if no 
+    #matches then return an empty array.
+    array_of_word.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
